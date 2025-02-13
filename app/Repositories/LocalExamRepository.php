6e92653913d8f94969afa5cab6d6662f56d6de0f
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2025-02-13 08:02:06 -0600

    ♻️ Refactorizar LocalExamRepository: eliminar la lógica de recuperación de preguntas y devolver nulo

--- app/Repositories/LocalExamRepository.php
+++ app/Repositories/LocalExamRepository.php
@@ -2,24 +2,13 @@
 
 namespace App\Repositories;
 
-use App\Collections\QuestionCollection;
 use App\Domain\Entities\QuestionEntity;
 use App\Domain\Repositories\ExamRepository;
-use Illuminate\Support\Facades\Storage;
 
 class LocalExamRepository extends ExamRepository
 {
     public function questions(?string $type): ?QuestionEntity
     {
-
-        $jsonContent = Storage::disk('local')->get(config('app.outputJson'));
-
-        $questionCollection = new QuestionCollection();
-
-        $questionCollection->fromJson($jsonContent);
-
-        return $questionCollection
-            ->when($type, fn($collection) => $collection->filterByCategory($type))
-            ->takeRandomInQuestions(5)->first();
+        return null;
     }
 }
