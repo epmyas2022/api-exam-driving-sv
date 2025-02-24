e85f00da063645f42ae11fc09afa75c8dc25ba22
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2025-02-24 07:31:56 -0600

    Add Tailwind CSS integration and new components: include Tailwind CSS dependencies, create Appbar and Checkbox components, and set up layout structure

---
+++ resources/views/components/appbar-component.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1 class="text-4xl font-bold text-center text-white font-anton p-10">
+        Exam Driving SV
+    </h1>
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <title>Exam driving</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+
+</head>
+
+<body class="text-white bg-gradient-to-r from-slate-900 to-slate-800 w-full h-dvh">
+
+    @yield('content')
+
+</body>
+
+</html>
